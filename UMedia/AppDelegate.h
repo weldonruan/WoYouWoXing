6fbd8bb4230df38f673aaa8ae8be1f70d71177ba
zt <apple@applematoMacBook-Pro.local>
2012-05-30 17:59:41 +0800

    Initial Commit

---
+++ UMedia/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UMedia
+//
+//  Created by  on 12-5-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
